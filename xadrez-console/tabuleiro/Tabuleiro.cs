ec7a92d88bcca8d8e7eadf7c583277dea578bfc0
Otavio Henrique <otaviohbp13@gmail.com>
2023-08-05 15:09:46 -0300

    Começando a coloacar peças no tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -25,5 +25,21 @@ namespace tabuleiro
         }
 
 
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
+
+            p.posicao = pos;
+        
+        
+        }
+
+
+
+
+
+
+
+
     }
 }
