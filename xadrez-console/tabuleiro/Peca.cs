6f8c09c48580d224c5ab6f414d27168b062e2cb4
Otavio Henrique <otaviohbp13@gmail.com>
2023-08-11 22:40:13 -0300

    Fazendo os priemiros testes de movimentação

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -21,5 +21,11 @@ namespace tabuleiro
             this.tab = tab;
             this.qteMovimentos = 0;
         }
+
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
+
     }
 }
